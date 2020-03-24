7a81e93d003108fe54fff631bfeb91aee4e92977
Ding Ma <ding.ma@mail.mcgill.ca>
2020-03-24 13:09:41 -0400

    writing to crn box doesnt work

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 const puppeteer = require('puppeteer');
 const config = require("../credentials");
 
+(async () => {
 
-(async ()=>{
 
-    let browser = await puppeteer.launch({headless:false});
+    let browser = await puppeteer.launch({headless: false});
     let page = await browser.newPage();
-
     await page.goto('https://horizon.mcgill.ca/pban1/twbkwbis.P_WWWLogin', {waitUntil: 'networkidle0'});
 
     //logs in
@@ -24,8 +23,20 @@ const config = require("../credentials");
     //navigates to quick add drop
     await page.waitForXPath("/html/body/div[3]/table[1]/tbody/tr[3]/td[2]/a", {waitUntil: 'networkidle0'}).then(selector => selector.click());
 
-    await page.waitFor(1000);
+    await page.waitFor(500);
     //selects the term
-    await page.hover('#term_id', config.semester, {delay: 30});
-    await page.click("#submit");
+    //await page.select('select[name=term_in]', JSON.stringify(config.semester), {delay: 30});
+    // await page.waitForXPath("/html/body/div[3]/form/table/tbody/tr[1]/td[2]/select/option[1]",{waitUntil: 'networkidle0'});
+
+    //#term_id > option:nth-child(1)
+
+    await page.waitForXPath("/html/body/div[3]/form/input", {waitUntil: 'networkidle0'}).then(selector => selector.click());
+
+    await page.type("#crn_id1", config.CRNs[0], {delay: 30});
+    // for (let i = 0; i <config.CRNs.length ; i++) {
+    //     let inputID = "#crn_id"+(i+1);
+    //     let crn = config.CRNs[i];
+    //     await page.type(inputID,crn,{delay: 30});
+    // }
+
 })();
