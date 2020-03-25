21b078f6b324019efd81364d6c4d1ad55f0d003f
Ding Ma <ding.ma@mail.mcgill.ca>
2020-03-25 11:06:52 -0400

    logging works with gcp.

--- src/gcp.js
+++ src/gcp.js
@@ -1,14 +1,13 @@
 const puppeteer = require('puppeteer');
 const config = {
     "email": "your.email@mail.mcgill.ca",
-    "password": "password",
+    "password": "pwd",
     "semester": "202005",
     "CRN": ["329", "327", "527", "528"]
-};
+}
 
 
 exports.handler = async (req, res) => {
-
 
     let browser = await puppeteer.launch({
         args: ['--no-sandbox'],
@@ -23,6 +22,7 @@ exports.handler = async (req, res) => {
     await page.click('#mcg_un_submit');
     await page.waitForNavigation({waitUntil: 'networkidle0'});
 
+    console.log("Login Successful");
     //navigation to student
     await page.waitForXPath("/html/body/div[1]/div[2]/span/map/table/tbody/tr[1]/td/table/tbody/tr/td[5]", {waitUntil: 'networkidle0'}).then(selector => selector.click());
 
@@ -37,7 +37,7 @@ exports.handler = async (req, res) => {
     await page.select('#term_id', config.semester);
     await page.waitForXPath("/html/body/div[3]/form/input", {waitUntil: 'networkidle0'}).then(selector => selector.click());
     await page.waitForNavigation();
-
+    console.log("Term " + config.semester + " found, Starting to input CRNs");
     //inputs all the crn and submits them
     for (let i = 0; i < config.CRN.length; i++) {
         let inputID = "#crn_id" + (i + 1);
@@ -48,9 +48,25 @@ exports.handler = async (req, res) => {
 
     try {
         await page.waitForSelector('.errortext', {timeout: 5000});
-        res.send("Registration failed - Class full");
+
+        const selector = 'body > div.pagebodydiv > form > table:nth-child(23)';
+        const table = await page.$$eval(selector, trs => trs.map(tr => {
+            const tds = [...tr.getElementsByTagName('td')];
+            return tds.map(td => td.textContent);
+        }));
+
+        let classesList = table[0];
+        let numbClass = classesList.length / 10;
+        console.log("Registration failed " + numbClass + " class(es) failed");
+        for (let i = 0; i < numbClass; i++) {
+            let classCrn = classesList[1 + i * 10];
+            let classSubj = classesList[2 + i * 10];
+            let classCode = classesList[3 + i * 10];
+            console.log("Failed to register to: CNR" + classCrn, classSubj + classCode);
+        }
     } catch (error) {
         // although this is inside a catch block, this is the case where registration was successful.
-        res.send("Successful registration");
+        console.log("Success! All classes were registered.");
     }
+    res.end();
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const puppeteer = require('puppeteer');
 const config = require("../credentials");
 
+//todo: log into a file instead
 (async () => {
 
 
@@ -13,6 +14,7 @@ const config = require("../credentials");
     await page.type('#mcg_pw', config.password, {delay: 30});
     await page.click('#mcg_un_submit');
     await page.waitForNavigation({waitUntil: 'networkidle0'});
+    console.log("Login Successful");
 
     //navigation to student
     await page.waitForXPath("/html/body/div[1]/div[2]/span/map/table/tbody/tr[1]/td/table/tbody/tr/td[5]", {waitUntil: 'networkidle0'}).then(selector => selector.click());
@@ -29,6 +31,7 @@ const config = require("../credentials");
     await page.waitForXPath("/html/body/div[3]/form/input", {waitUntil: 'networkidle0'}).then(selector => selector.click());
     await page.waitForNavigation();
 
+    console.log("Term " + config.semester + " found, Starting to input CRNs");
     //inputs all the crn and submits them
     for (let i = 0; i < config.CRN.length; i++) {
         let inputID = "#crn_id" + (i + 1);
@@ -39,9 +42,24 @@ const config = require("../credentials");
 
     try {
         await page.waitForSelector('.errortext', {timeout: 5000});
-        console.log("Registration failed - Class full");
+
+        const selector = 'body > div.pagebodydiv > form > table:nth-child(23)';
+        const table = await page.$$eval(selector, trs => trs.map(tr => {
+            const tds = [...tr.getElementsByTagName('td')];
+            return tds.map(td => td.textContent);
+        }));
+
+        let classesList = table[0];
+        let numbClass = classesList.length / 10;
+        console.log("Registration failed " + numbClass + " class(es) failed");
+        for (let i = 0; i < numbClass; i++) {
+            let classCrn = classesList[1 + i * 10];
+            let classSubj = classesList[2 + i * 10];
+            let classCode = classesList[3 + i * 10];
+            console.log("Failed to register to: CNR" + classCrn, classSubj + classCode);
+        }
     } catch (error) {
           // although this is inside a catch block, this is the case where registration was successful.
-        console.log("Successful registration");
+        console.log("Success! All classes were registered.");
       }
 })();
