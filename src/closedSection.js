541ee0c93a78dfc4c8db4e91a517870a06882ef2
yiyiyang81 <57012409+yiyiyang81@users.noreply.github.com>
2020-08-27 00:14:51 +0800

    Inital commit of closed section

---
+++ src/closedSection.js
@@ -0,0 +1,89 @@
+const puppeteer = require('puppeteer');
+const sgMail = require('@sendgrid/mail');
+
+/*
+EDIT THE CONFIG BELOW BEFORE RUNNING
+
+This function checks from the course search if the class is not full.
+
+email: McGill Minerva username
+password: McGill Minerva password
+Term - Term you wish to register for, found in VSB URL (ex. term=202005)
+CRN - List of CRNs you wish to register for, found in VSB at bottom of screen
+url - Select the classes you want on VSB for your selected term then copy the URL over
+wantEmail - Set to true if you want an email notification upon successful registration
+notifEmail - Where to send notification email to, leave blank if wantEmail is false
+sgApiKey - Received when registered for sendgrid, leave blank is wantEmail is false
+*/
+
+const config = {
+    "regEmail": "",
+    "password": "",
+    "term": "202009",
+    "CRN": ["18276"],
+    "subject": "COMP",
+    "courseNumber": "310",
+    "url": "https://vsb.mcgill.ca/vsb/criteria.jsp?access=0&lang=en&tip=1&page=results&scratch=0&term=202009&sort=none&filters=iiiiiiiii&bbs=&ds=&cams=Distance_Downtown_Macdonald_Off-Campus&locs=any&isrts=&course_0_0=COMP-310&sa_0_0=&cs_0_0=--202009_18276--&cpn_0_0=&csn_0_0=&ca_0_0=&dropdown_0_0=us_--202009_18276--&ig_0_0=0&rq_0_0=",
+    "wantEmail": true,
+    "notifEmail": "",
+    "sgApiKey": "SG.xxx..."
+};
+
+async function determineRegisterableClass(page) {
+
+    console.log("Logging in...");
+
+    await page.goto('https://horizon.mcgill.ca/pban1/twbkwbis.P_WWWLogin', {waitUntil: 'networkidle0'});
+
+    //logs in
+    await page.type('#mcg_un', config.regEmail, {delay: 30});
+    await page.type('#mcg_pw', config.password, {delay: 30});
+    await page.click('#mcg_un_submit');
+    await page.waitForNavigation({waitUntil: 'networkidle0'});
+
+    console.log("Login Successful");
+
+    await page.goto("https://horizon.mcgill.ca/pban1/bwskfcls.p_sel_crse_search", {waitUntil: 'networkidle0'})
+
+    //selects the term and navigates to next page
+    await page.select('#term_id', config.term);
+    await page.waitForXPath("/html/body/div[3]/form/input", {waitUntil: 'networkidle0'}).then(selector => selector.click());
+    await page.waitForNavigation();
+    console.log("Term " + config.term + " found, proceed to subject");
+
+    //selects the Subject and search for course\
+    await page.select('#sub_id', config.subject);
+    await page.waitForXPath("/html/body/div[3]/form/input[17]", {waitUntil: 'networkidle0'}).then(selector => selector.click());
+    await page.waitForNavigation();
+    console.log("Subject " + config.subject + " found, proceed to select course");
+
+    //select the course and proceed to check availability
+    var parentElement;
+    for(let i = 0; i<document.length; i++){
+        let navigator = document.querySelectorAll('input[name=SEL_CRSE]')[i]
+        if(navigator == config.courseNumber){
+            parentElement  = navigator.parentElement
+            break
+        }
+    }
+    for(let i = 0; i<document.length; i++){
+        let navigator = parentElement.childElement[i]
+        if(navigator.name == "SUB_BTN"){
+            navigator.click()
+        }
+    }
+    await page.waitForNavigation();
+    console.log("Course " + config.subject +config.courseNumber+ " found, proceed to check availability");
+
+
+    //check if the section is full
+    if(document.querySelectorAll('abbr')[13].title != "Closed"){
+        selector => selector.click() //or document.querySelectorAll('input[value=Register]')[0].click()
+    }
+
+
+    //checks if there are errors after registration
+
+    return ;
+}
+
