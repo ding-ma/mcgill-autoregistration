19bcff43bd0a175cafac537dc19215836aabf227
yiyiyang81 <57012409+yiyiyang81@users.noreply.github.com>
2021-01-11 00:43:57 +0800

    Added waitlist notification feature (#12)
    * Add feature
    
    Wait list notification through Minerva
    
    * Update index.js
    
    * Update function.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const {multiclass} = require("./multiclass/function");
 const {waitlist} = require("./waitlist")
 const {closedSection} = require("./closedSection")
+const {waitlistNotify} = require("./waitlistNotify")
 
 // boiler code to call the Registration function
 multiclass()
